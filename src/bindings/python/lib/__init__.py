218f0cabac0e22899483b475408a6b61587fdd0a
Bruno Dupuis <lisael@lisael.org>
2015-06-27 22:38:41 +0200

    removed python bindings (still in git@github/lisael/pylogicaldecoding.git)

--- src/bindings/python/lib/__init__.py
+++
@@ -1,7 +0,0 @@
-from logicaldecoding._logicaldecoding import Reader as _Reader
-
-
-class Reader(_Reader):
-
-    def event(self, value):
-        raise NotImplementedError("event must be overridden in client")
