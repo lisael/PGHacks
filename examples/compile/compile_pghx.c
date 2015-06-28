cf0f579366392a55121a6cd8012c67bc37cb6f92
Bruno Dupuis <lisael@lisael.org>
2015-06-28 19:08:44 +0200

    added examples dir

---
+++ examples/compile/compile_pghx.c
@@ -0,0 +1,22 @@
+/* Just a simple executable to check install and give an example of
+ * Makefile
+ */
+#include <logicaldecoding.h>
+#include <stdlib.h>
+
+int
+main(void)
+{
+    pghx_ld_reader r;
+    pghx_ld_reader *pr = &r;
+    int res;
+
+    res = pghx_ld_reader_init(pr);
+    if (!res){
+        puts("Did compile but doesn't work");
+        return(1);
+    }
+    puts("It works!");
+    return 0;
+}
+
